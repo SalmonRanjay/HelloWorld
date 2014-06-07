217d1ffb3fdffed363bfc322bc8115ad62af2064
Ranjay <ranjy23@gmail.com>
2014-06-07 18:20:52 -0400

    Files

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/stylesheets/index.css"/>
+	</head>
+	<body>
+		<div id="container">
+			<div id="banner"> 
+				<h1 id="banner_h1"> This site is under Maintennace </h1>
+			</div>
+
+			<div id="top_menu" >
+				<ul id="top_ul">
+					<li class="top_nav"> <a class="top_nav_link" href="#home">Home </a> </li>
+					<li class="top_nav"> <a class="top_nav_link" href="#About">About</a> </li>
+					<li class="top_nav"> <a class="top_nav_link" href="#Resume">Resume</a> </li>
+					<li class="top_nav"> <a class="top_nav_link" href="#gallery">Gallery</a> </li>
+					<li class="top_nav"> <a class="top_nav_link" href="#contact">Contact</a> </li>
+					<li class="top_nav"> <a class="top_nav_link" href="#contact">WEB APP</a> </li>
+				</ul>
+			</div>
+			<div id="left_container" class="contentArea">
+					
+				<ul id="left_ul">
+					<li class="left_nav"> <a class="left_nav_link" href="#home">Home </a> </li>
+					<li class="left_nav"> <a class="lft_nav_link" href="#About">About</a> </li>
+					<li class="left_nav"> <a class="left_nav_link" href="#Resume">Resume</a> </li>
+					<li class="left_nav"> <a class="left_nav_link" href="#gallery">Gallery</a> </li>
+					<li class="left_nav"> <a class="left_nav_link" href="#contact">Contact</a> </li>
+					<li class="top_nav"> <a class="top_nav_link" href="#contact">WEB APP</a> </li>
+				</ul>
+
+
+			</div>
+			<div id="right_container" class="contentArea">
+				<div id="map_container" class="contentArea">
+				</div>
+
+				<div id="middle_area" class="contentArea" >
+				</div>
+
+				<div id="right_area" class="contentArea">
+
+				</div>	
+			</div>
+			<div id="footer" class="contentArea">
+	
+			</div>
+		</div>
+	</body
+</html>
---
+++ testpage.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head> <link type="text/css" rel="stylesheet" href="/stylesheets/testpage.css"/> </head>
+	<body>
+		<div id="container"> 
+			<div id="heading">  <h1> Creativity Is Sometimes Best Left Hidden </h1>  </div>
+
+			<div id="menu_container">	
+				<ul id="menu_bar">
+					<li class="top_navbar"> <a class="top_navlink" href="#home">Home</a></li>
+			        	<li class="top_navbar"><a class="top_navlink" href="#News">News </a></li>
+					<li class="top_navbar"><a class="top_navlink" href="#Contact">Contact</a></li>
+					<li class="top_navbar"><a class="top_navlink" href="#about">About</a></li>
+					<li class="top_navbar"><a class="top_navlink" href="#App">App Management</a></li>
+					<li class="top_navbar"><a class="top_navlink" href="#resume">Resume</a></li>
+					<li class="top_navbar"><a class="top_navlink" href="#biography">Biography</a></li>
+					<li class="top_navbar"><a class="top_navlink" href="#gallery">Gallery</a></li>
+				</li>
+			</div>
+			<div id="left_column">	
+				<ul id="side_menu">
+					<li class="side_navbar"> <a class="side_navlink" href="#home">Home</a></li>
+			        	<li class="side_navbar"><a class="side_navlink" href="#News">News </a></li>
+					<li class="side_navbar"><a class="side_navlink" href="#Contact">Contact</a></li>
+					<li class="side_navbar"><a class="side_navlink" href="#about">About</a></li>
+					<li class="side_navbar"><a class="side_navlink" href="#App">App Management</a></li>
+					<li class="side_navbar"><a class="side_navlink" href="#resume">Resume</a></li>
+					<li class="side_navbar"><a class="side_navlink" href="#biography">Biography</a></li>
+					<li class="side_navbar"><a class="side_navlink" href="#gallery">Gallery</a></li>
+				</ul>
+		
+			</div>
+			<div id="right_column">
+					<div id="map_container" class="right_containers"> 
+						<img src="/images/jamaciaflag.jpg" width="300" height="500">
+					 </div>
+					<div id="video_container" class="right_containers">
+						<p>Content Coming Soon</p>	
+					</div>
+	
+					<div id="right_container" class="right_containers"> 
+						<img src="/images/iamjam.jpeg" width="381.5" height="500" style=" margin-bottom: 0px">
+					 </div>
+				</div>
+			<div id="footer">
+				<p id="footer_text"> this page is under maintennace</p>
+			</div>
+		</div>
+	</body>
+</html>
