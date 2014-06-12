978abb1fcfb4bbb950a1f41884175c788447f5f0
Ranjay Salmon <ranjy23@yahoo.com>
2014-06-12 05:20:48 -0400

    updated page layout

--- index.php
+++ index.php
@@ -1,51 +1,83 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" >
 <html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="/stylesheets/index.css"/>
-	</head>
-	<body>
-		<div id="container">
-			<div id="banner"> 
-				<h1 id="banner_h1"> This site is under Maintennace </h1>
-			</div>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+	<title> Coder inc...</title>
+	<meta http-equiv="Content-Language" content="en-us" />
 
-			<div id="top_menu" >
-				<ul id="top_ul">
-					<li class="top_nav"> <a class="top_nav_link" href="#home">Home </a> </li>
-					<li class="top_nav"> <a class="top_nav_link" href="#About">About</a> </li>
-					<li class="top_nav"> <a class="top_nav_link" href="#Resume">Resume</a> </li>
-					<li class="top_nav"> <a class="top_nav_link" href="#gallery">Gallery</a> </li>
-					<li class="top_nav"> <a class="top_nav_link" href="#contact">Contact</a> </li>
-					<li class="top_nav"> <a class="top_nav_link" href="#contact">WEB APP</a> </li>
-				</ul>
-			</div>
-			<div id="left_container" class="contentArea">
-					
-				<ul id="left_ul">
-					<li class="left_nav"> <a class="left_nav_link" href="#home">Home </a> </li>
-					<li class="left_nav"> <a class="lft_nav_link" href="#About">About</a> </li>
-					<li class="left_nav"> <a class="left_nav_link" href="#Resume">Resume</a> </li>
-					<li class="left_nav"> <a class="left_nav_link" href="#gallery">Gallery</a> </li>
-					<li class="left_nav"> <a class="left_nav_link" href="#contact">Contact</a> </li>
-					<li class="top_nav"> <a class="top_nav_link" href="#contact">WEB APP</a> </li>
-				</ul>
+	<meta http-equiv="imagetoolbar" content="no" />
+	<meta name="MSSmartTagsPreventParsing" content="true" />
 
-
-			</div>
-			<div id="right_container" class="contentArea">
-				<div id="map_container" class="contentArea">
-				</div>
-
-				<div id="middle_area" class="contentArea" >
-				</div>
-
-				<div id="right_area" class="contentArea">
-
-				</div>	
-			</div>
-			<div id="footer" class="contentArea">
+	<meta name="description" content="Hello world appengine example" />
+	<meta name="keywords" content="Helloworld, example program, helloexampletest" />
 	
+	<meta name="author" content="Ranjay salmon" />
+
+	<link rel="stylesheet" type="text/css" href="/stylesheets/index.css" />
+
+</head>
+
+	<body>
+		<div id="page-container">
+			<div id="main-nav">
+				<dl >
+					<dt id="about"><a href="#">About </a></dt>
+					<dt id="services"><a href="#">Resume</a></dt>
+					<dt id="portfolio"><a href="#">Web APP</a></dt>
+					<dt id="contact"><a href="#">Contact</a></dt>
+				</dl>	
+			 </div>
+
+			<div id="header">
+				
+			 	
 			</div>
+				
+			
+			<div id="sidebar-a">
+				 <div class="padding">
+					<p>" I have not failed. I've just found 10,000 ways that wont work." <br/>
+					Thomas A. Edison.</p>
+
+					<p>"Our Greatest Weakness lies in giving up. The most certain way to succeed is always to try just one more time."<br/>
+					Thomas A. Edison.</p>
+
+					<p>"You must be the change you wish to see in the world <br/>
+					Mahatma Gandhi.<br/>
+					</p>
+
+					
+					
+					
+				</div> 
+			</div>
+
+			<div id="content">
+				<div class="padding"> 
+					<h2> <img src="/images/about.gif"width="54" height="14" alt="About" /> </h2>
+					<p><strong> Coder inc... </strong>is Test webpage used to display the front and back end developement skills of its designers Ranjay Salmon. Content on this webpage will be updated periodically to display Projects developed by the websites author. </p>
+					<p> This webpage is currently undergoing maintennace so if you have any questions or would like more information about the developers Skills or proficiency please feel free to reach out and make contact me. </p>
+					<h2> <img src="/images/contact.gif" width="98" height="14" alt="Contact" /> </h2>
+					<p>Phone: 301-213-3771 <br/>
+					Email: <a href="mailto:ranjy23@gmail.com">Ranjy23@gmail.com</a> </br>
+					Maryland, md, 20784 </p>
+				</div>
+			 </div>
+
+			<div id="footer"> 
+				<div id="altnav">
+					<a href="#"> About</a>-
+					<a href="#">Resume</a> -
+					<a href="#">Web APP </a> -
+					<a href=""> Contac Us </a> -
+					<a href="#">Terms of Trade</a>
+					</div>
+					Copyright cCoder inc....
+					Powered by <a href="http://appengine.google.com">Google Appengine </a>
+			 </div>
 		</div>
-	</body
-</html>
+
+	</body>
+</html> 
+	
+
